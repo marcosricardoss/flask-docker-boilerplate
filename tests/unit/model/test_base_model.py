52c5b567f9e8a39a195cce8b78291b8c855b7f04
Marcos Ricardo <marcosricardoss@gmail.com>
2020-06-19 13:51:53 -0300

    test: update Model.py test

--- tests/unit/model/test_base_model.py
+++ tests/unit/model/test_base_model.py
@@ -1,17 +1,20 @@
 """It contains tests for the Model class from the models.py module"""
 
 import pytest
-from sqlalchemy.exc import NoInspectionAvailable
 
-def test_base_model(session):
+def test_base_model(session, mocker):
     """
     GIVEN a User model
     WHEN a new User is created
     THEN check the serialize() and remove_session() methods
     """
 
+    import sqlalchemy
     from app.model.models import Model
+    
+    mocker.patch('sqlalchemy.inspect')
+
     model = Model()
     assert model.serialize() == {}   
-    with pytest.raises(NoInspectionAvailable):
-        model.remove_session()
+    model.remove_session()
+    sqlalchemy.inspect.assert_called_once()
